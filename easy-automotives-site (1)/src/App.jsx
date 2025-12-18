9718014eb56dd7b818c25043e0842b20bd36527f
Akiiooo19 <cesaretorresss19@gmail.com>
2025-12-17 19:27:22 -0700

    Add files via upload

---
+++ easy-automotives-site (1)/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: 40 }}>
+      <h1>Easy Automotives</h1>
+      <p>Premium vehicles. A clear, easy buying experience.</p>
+      <p>This site is successfully deployed 🚀</p>
+    </div>
+  )
+}
