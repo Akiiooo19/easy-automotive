b6d2389857e5b0742d83ee55bbe0a72c5f14e334
Akiiooo19 <cesaretorresss19@gmail.com>
2025-12-17 21:37:59 -0700

    Delete easy-automotives-site (1) directory

--- easy-automotives-site (1)/src/App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', padding: 40 }}>
-      <h1>Easy Automotives</h1>
-      <p>Premium vehicles. A clear, easy buying experience.</p>
-      <p>This site is successfully deployed 🚀</p>
-    </div>
-  )
-}
